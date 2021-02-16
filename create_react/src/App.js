47af53d24478aa2aa23996f02603a8055d534f64
JeongHyeon Lee <dlqjdgus99@naver.com>
2021-02-17 01:55:41 +0900

    feat: third_assignment_react_without-create-react-app

---
+++ create_react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    render(){
+        return(
+            <div>
+                <h1>React App</h1>
+            </div>
+        );
+    }
+}
+export default App;
